265eb1f988ca88c0f7bd13e80dca1e048cb02fc5
Manish Prajapati <manishprajapatim416@gmail.com>
2023-11-21 23:56:24 +0530

    Added products name and ratings

--- screens/ProductDetailsScreen.tsx
+++ screens/ProductDetailsScreen.tsx
@@ -1,9 +1,11 @@
 import { RouteProp } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { FC, useState } from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
+import StarRating from "react-native-star-rating-widget";
 import CartButton from "../components/CartButton";
 import GoBackButton from "../components/GoBackButton";
+import { ManropeText } from "../components/StyledText";
 import Colors from "../constants/Colors";
 
 const ProductDetailsScreen: FC<ProductDetailsScreenProps> = ({ route }) => {
@@ -16,6 +18,7 @@ const ProductDetailsScreen: FC<ProductDetailsScreenProps> = ({ route }) => {
       }}
     >
       <Header />
+      <ProductDetails product={_product} />
     </View>
   );
 };
@@ -31,6 +34,63 @@ const Header = () => {
     >
       <GoBackButton />
       <CartButton variant="dark" />
+    </View>
+  );
+};
+
+const ProductDetails = ({ product }: { product: Product }) => {
+  return (
+    <View
+      style={{
+        paddingHorizontal: 30,
+      }}
+    >
+      <View
+        style={{
+          marginTop: 20,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 30,
+            opacity: 0.8,
+          }}
+        >
+          {product.title}
+        </Text>
+        {product?.brand && (
+          <Text
+            style={{
+              fontSize: 30,
+              fontWeight: "bold",
+            }}
+          >
+            {product.brand}
+          </Text>
+        )}
+      </View>
+      <View
+        style={{
+          marginTop: 10,
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 10,
+        }}
+      >
+        <StarRating
+          rating={product.rating}
+          onChange={() => {}}
+          starSize={25}
+          style={{
+            padding: 0,
+            margin: 0,
+            marginLeft: -10,
+          }}
+        />
+        <ManropeText style={{ fontSize: 20, opacity: 0.5 }}>
+          {Math.floor(Math.random() * 100)} Reviews
+        </ManropeText>
+      </View>
     </View>
   );
 };
