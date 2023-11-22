2cd4bbe93a5ffadd5a3ce8b5f19cc3af2d0f8147
Manish Prajapati <manishprajapatim416@gmail.com>
2023-11-22 15:32:21 +0530

    Added count to cart icon

--- components/CartButton.tsx
+++ components/CartButton.tsx
@@ -1,7 +1,8 @@
-import { FC } from "react";
-import { ShoppingBag, ShoppingBasket } from "lucide-react-native";
-import { TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { ShoppingBag } from "lucide-react-native";
+import { FC, useMemo, useContext } from "react";
+import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
+import { ApplicationContext } from "../ApplicationContext";
 import Colors from "../constants/Colors";
 
 interface CartButtonProps {
@@ -9,27 +10,51 @@ interface CartButtonProps {
 }
 
 const CartButton: FC<CartButtonProps> = ({ variant = "light" }) => {
+  const { productsInCart } = useContext(ApplicationContext);
+  const count = useMemo(() => {
+    return Object.keys(productsInCart).length;
+  }, [productsInCart]);
   const navigation: any = useNavigation();
+
   const navigateToCartScreen = () => {
     navigation.navigate("CartScreen");
   };
+
   return (
-    <TouchableOpacity
-      onPress={() => navigateToCartScreen()}
-      style={{
-        height: 50,
-        width: 50,
-        borderRadius: 25,
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <ShoppingBag
-        size={25}
-        color={variant === "light" ? "white" : Colors.black100}
-      />
-    </TouchableOpacity>
+    <View style={{ position: "relative" }}>
+      <TouchableOpacity
+        onPress={() => navigateToCartScreen()}
+        style={styles.cartIconButton}
+      >
+        <ShoppingBag
+          size={25}
+          color={variant === "light" ? "white" : Colors.black100}
+        />
+      </TouchableOpacity>
+      <Text style={styles.countText}>{count}</Text>
+    </View>
   );
 };
 
 export default CartButton;
+
+const styles = StyleSheet.create({
+  cartIconButton: {
+    height: 50,
+    width: 50,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  countText: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    backgroundColor: "#F9B023",
+    color: Colors.white,
+    padding: 4,
+    paddingHorizontal: 8,
+    borderRadius: 15,
+    fontSize: 15,
+  },
+});
